b24c58fa4010cfe7e5aadc58c39ab236b4afa090
kaai <kaainars@gmail.com>
2023-11-22 19:30:11 +0200

    third commit

---
+++ src/components/shared/LeftSidebar.tsx
@@ -0,0 +1,12 @@
+
+const LeftSidebar = () => {
+  return (
+    <nav className="hidden md:flex px-6 py-10 flex-col justify-between min-w-[270px] bg-dark-2">
+      <div className="flex flex-col gap-11">
+
+      </div>
+    </nav>
+  )
+}
+
+export default LeftSidebar
